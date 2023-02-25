7fa5db5ee301c5907b0022d164ab8d468aa8f30f
Thuy Duong <vuhoang.thduong@gmail.com>
2023-02-25 10:09:04 +0100

    update main to test other function

--- BASH/main.c
+++ BASH/main.c
@@ -16,9 +16,7 @@ int main(void) {
   printf("%s\n",str);
 	int init_size = (int)strlen(str);
 	char* delim = " ";
-
 	char *ptr = strtok(str, delim);
-
 	while (ptr != NULL) {
 		printf("'%s'\n", ptr);
 		ptr = strtok(NULL, delim);
@@ -31,6 +29,23 @@ int main(void) {
   } else {
     perror("getcwd() error");
     return 1;
+  }
+
+  int ret;
+  FILE *fp;
+  char filename[] = "file.txt";
+
+  fp = fopen(filename, "w");
+
+  fprintf(fp, "%s", "This is tutorialspoint.com");
+  fclose(fp);
+   
+  ret = remove(filename);
+
+  if(ret == 0) {
+    printf("File deleted successfully");
+  } else {
+    printf("Error: unable to delete the file");
   }
 
 	return 0;
