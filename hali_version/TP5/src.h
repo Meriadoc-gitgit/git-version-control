6dff86b14b4e12c7452bdf5d8b9d0ca6bf91d72c
Thuy Duong <vuhoang.thduong@gmail.com>
2023-03-02 14:50:34 +0100

    update co-member' version

---
+++ hali_version/TP5/src.h
@@ -0,0 +1,14 @@
+#ifndef _SRC
+#define _SRC
+
+#include "cell.h"
+
+int hashFile(char *source, char *dest);
+char* sha256file(char* file);
+List* listdir(char* root_dir);
+int file_exists(char *file);
+void cp(char *to, char *from);
+char* hashToPath(char* hash);
+void blobFile(char* file);
+
+#endif
